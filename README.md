596ea3fa1116715a161323b28dbe60f022e10249
Yuliya be-Code <yuliabochkovskaya@gmail.com>
2021-02-24 12:26:24 +0100

    js files added

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 ### Welcome to my RPG game 
 
+[Link to the game](https://yuliya-becode.github.io/js-rpg/index.html)
+
 #### Creation of characters
 [source](http://gaurav.munjal.us/Universal-LPC-Spritesheet-Character-Generator/)
---
+++ character.js
@@ -0,0 +1,45 @@
+window.onload = function () {
+    let heroOne = {
+        name: 'John',
+        armor: 0.1,
+        agility: 1,
+        health: 100,
+        type: '',
+        money: 0,
+        imageUrl: ''
+
+    }
+
+     let heroSecond = {
+        name: 'Bob',
+        armor: 0.1,
+        agility: 1,
+        health: 100,
+        type: '',
+        money: 0,
+        imageUrl: ''
+
+    }
+
+     let enemyOne = {
+        name: 'Bad',
+        armor: 0.1,
+        agility: 1,
+        health: 100,
+        type: '',
+        money: 0,
+        imageUrl: ''
+
+    }
+
+     let enemySecond = {
+        name: 'veryBad',
+        armor: 0.1,
+        agility: 1,
+        health: 100,
+        type: '',
+        money: 0,
+        imageUrl: ''
+
+    }
+}
